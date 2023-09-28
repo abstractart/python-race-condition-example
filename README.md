97181636f8aef0264411381b48d657c05ef72e61
Eugene Kozlov <ev.kozlov@tinkoff.ru>
2023-09-28 21:51:35 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Race condition in Python 3
+
+## Quick start
+- Install dependencies:
+    - pyenv
+    - python version for testing
+- Specify versions in `script.sh`
+- Run `./script.sh`
---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+versions=(3.7 3.8 3.9 3.10 3.11)
+
+for version in ${versions[*]}
+do
+    pyenv shell $version
+    python3 --version
+
+    python3 main.py
+    echo "\n"
+done
